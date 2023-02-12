adf60018cc79a3e3ccfee3e42af488084ccb6c65
Daniel Hromada <hromada.dan@gmail.com>
2023-02-12 17:50:34 +0100

    Search in 1mo old history and tweak icons

--- popup.js
+++ popup.js
@@ -1,10 +1,19 @@
 chrome.history.search(
-  { text: "", maxResults: 100_000_000 },
+  {
+    text: "",
+    maxResults: 100_000_000,
+    startTime: Date.now() - 1000 * 60 * 60 * 24 * 30 * 2, // 2 months ago
+    endTime: Date.now() - 1000 * 60 * 60 * 24 * 30 * 1, // 1 month ago
+  },
   function redirectCurrentTabToRandomHistoryItemURL(data) {
+    console.info(`Found ${data.length} history items.`);
     var randomHistoryItem = data[Math.floor(Math.random() * data.length)];
     var randomHistoryItemURL = randomHistoryItem.url;
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      console.info(`Redirecting current tab to ${randomHistoryItemURL}.`);
       chrome.tabs.update(tabs[0].id, { url: randomHistoryItemURL });
+      debugger;
+      window.close();
     });
   }
 );
